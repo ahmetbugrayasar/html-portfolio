0d4556835536fde54f2e2b4b84cf0d95b2485a26
ahmetbugrayasar <59139830+ahmetbugrayasar@users.noreply.github.com>
2023-07-19 22:04:15 +0300

    Added Final Score

--- dice-throw/index.js
+++ dice-throw/index.js
@@ -10,7 +10,8 @@ var title = document.createElement("h1");
 title.innerHTML = "Keliron Darthang Presents";
 diceContainer.appendChild(title);
 
-for (var i = 1; i <= diceCount; i++) {
+for (var i = 1; i <= diceCount; i++)
+{
     var diceDiv = document.createElement("div");
     diceDiv.className = "dice";
 
@@ -26,13 +27,24 @@ for (var i = 1; i <= diceCount; i++) {
     diceDiv.appendChild(diceImage);
 
     diceContainer.appendChild(diceDiv);
-  }
+}
 
+var totalCount = 0;
 for (var i = 1; i <= diceCount; i++)
 {
     var randomNumber = Math.round(Math.random() * (faceCount - 1)) + 1;
+    totalCount += randomNumber;
     diceValues.push(randomNumber);
     document.getElementsByClassName("img" + i)[0].src = "images/dice" + randomNumber + ".png";
 }
+var scoreDiv = document.createElement("div");
+scoreDiv.className = "container";
+var finalScore = document.createElement("p");
+finalScore.className="DiceText";
+scoreDiv.appendChild(finalScore);
+finalScore.innerHTML = "Final Score: "+totalCount.toString();
+
+// Append the scoreDiv to the diceContainer
+diceContainer.appendChild(scoreDiv);
 
 });
